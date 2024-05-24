1c16f24122e402f9310d8b6106f16981285eca04
rhol <rhol@student.codam.nl>
2024-05-24 15:39:12 +0200

    trying to add readline history

--- srcs/main.c
+++ srcs/main.c
@@ -4,20 +4,29 @@ int main(void)
 {
 	char *user_input;
 	int i = 0;
-	
+	HIST_ENTRY **the_history_list;
+
+	// test getenv.
 	printf("PATH : %s\n", getenv("PATH"));
 	printf("HOME : %s\n", getenv("HOME"));
 	printf("ROOT : %s\n", getenv("ROOT"));
 	
 	// test readline.
+	using_history();
 	while(i < 3)
 	{
 		printf("%d - ", i);
 		user_input = readline("Enter text: ");
 		add_history(user_input);
 		printf("%s", user_input);
-		printf("\n");
+		printf("\n\n");
 		i++;
 	}
+	// display history?
+	// HIST_ENTRY **history_list(void)
+	history_get_history_state();
+	printf("pointer to hist list: %p\n", &the_history_list);
+	// printf("history entry: %s\n", the_history_list->hist_entry);
+	// printf("%s\n", the_history_list[0][0]);
 	return (0);
 }
