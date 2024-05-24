6b4019d90b4103e0f9f941d183c9521dce99b654
rhol <rhol@student.codam.nl>
2024-05-24 15:25:11 +0200

    add first attempt of get_env vars

--- includes/header.h
+++ includes/header.h
@@ -1,8 +1,8 @@
-
 #ifndef SHELL_HEAD
 # define SHELL_HEAD
 
 # include <stdio.h>
+# include <stdlib.h>
 # include <readline/readline.h>
 # include <readline/history.h>
 
--- srcs/main.c
+++ srcs/main.c
@@ -4,8 +4,15 @@ int main(void)
 {
 	char *user_input;
 	int i = 0;
+	
+	printf("PATH : %s\n", getenv("PATH"));
+	printf("HOME : %s\n", getenv("HOME"));
+	printf("ROOT : %s\n", getenv("ROOT"));
+	
+	// test readline.
 	while(i < 3)
 	{
+		printf("%d - ", i);
 		user_input = readline("Enter text: ");
 		add_history(user_input);
 		printf("%s", user_input);
