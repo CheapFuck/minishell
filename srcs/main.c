2b922c6a790f42e666cf11b0203d709c699e104b
rhol <rhol@student.codam.nl>
2024-05-24 17:36:41 +0200

    copy some crap to this branch

--- srcs/main.c
+++ srcs/main.c
@@ -33,3 +33,36 @@ int main(void)
 	// printf("%s\n", the_history_list[0][0]);
 	return (0);
 }
+
+
+
+// int main() {
+//     char *input;
+//     int status ;
+    
+//     while(1)
+//     {
+//         input = readline("myshell> ");
+//     if (input && *input)
+//         add_history(input);
+//     // printf("input = %s\n", input);
+//     // status = system(input);
+//     if (ft_strncmp(input, "echo -n", ft_strlen(input)) == 0)
+//         printf("echo min n\n");
+//     if (ft_strncmp(input, "cd", ft_strlen(input)) == 0)
+//         printf("cd\n");
+//     if (ft_strncmp(input, "pwd", ft_strlen(input)) == 0)
+//         printf("pwd\n");
+//     if (ft_strncmp(input, "export", ft_strlen(input)) == 0)
+//         printf("export\n");
+//     if (ft_strncmp(input, "unset", ft_strlen(input)) == 0)
+//         printf("unset\n");
+//     if (ft_strncmp(input, "env", ft_strlen(input)) == 0)
+//         printf("env\n");
+//     if (ft_strncmp(input, "exit", ft_strlen(input)) == 0)
+//         printf("exit\n");
+//     }
+//     free(input);
+
+//     return 0;
+// }
