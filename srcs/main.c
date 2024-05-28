670878916f43eb8afbeefa4f8f216be20984a542
rhol <rhol@student.codam.nl>
2024-05-28 15:32:55 +0200

    first split on user input

--- srcs/main.c
+++ srcs/main.c
@@ -2,64 +2,29 @@
 
 int main(void)
 {
-	char *user_input;
-	int i = 0;
-	HIST_ENTRY **the_history_list;
+	int			status = -1;
+	char		*user_input;
+	char		**args = NULL;
+	int			i = 0;
+	int			j = 0;
+	// HIST_ENTRY	**the_history_list;
 
-	// add signal mask
-	// initialize_signals();
-
-	// test getenv.
-	printf("PATH : %s\n", getenv("PATH"));
-	printf("HOME : %s\n", getenv("HOME"));
-	printf("ROOT : %s\n", getenv("ROOT"));
-	
-	// test readline.
 	using_history();
-	while(1)
+	while(status == -1)
 	{
-		printf("%d - ", i);
-		user_input = readline("Enter text: ");
+		printf("%d-", i);
+		user_input = readline(": ");
+		args = split_line(user_input);
+		while (args[j])
+		{
+			printf("args[%d]: %s\n", j, args[j]);
+			j++;
+		}
+		j = 0;
 		add_history(user_input);
-		printf("%s", user_input);
-		printf("\n\n");
+		printf("\n");
 		i++;
 	}
-	// history_get_history_state();
-	printf("pointer to hist list: %p\n", &the_history_list);
-	// printf("history entry: %s\n", the_history_list->hist_entry);
 	return (0);
 }
 
-
-
-// int main() {
-//     char *input;
-//     int status ;
-    
-//     while(1)
-//     {
-//         input = readline("myshell> ");
-//     if (input && *input)
-//         add_history(input);
-//     // printf("input = %s\n", input);
-//     // status = system(input);
-//     if (ft_strncmp(input, "echo -n", ft_strlen(input)) == 0)
-//         printf("echo min n\n");
-//     if (ft_strncmp(input, "cd", ft_strlen(input)) == 0)
-//         printf("cd\n");
-//     if (ft_strncmp(input, "pwd", ft_strlen(input)) == 0)
-//         printf("pwd\n");
-//     if (ft_strncmp(input, "export", ft_strlen(input)) == 0)
-//         printf("export\n");
-//     if (ft_strncmp(input, "unset", ft_strlen(input)) == 0)
-//         printf("unset\n");
-//     if (ft_strncmp(input, "env", ft_strlen(input)) == 0)
-//         printf("env\n");
-//     if (ft_strncmp(input, "exit", ft_strlen(input)) == 0)
-//         printf("exit\n");
-//     }
-//     free(input);
-
-//     return 0;
-// }
--- srcs/utils.c
+++ srcs/utils.c
@@ -1,25 +1,38 @@
 #include "header.h"
 
-// void	handle_signal(int signal, siginfo_t *info, void *ucontext)
-// {
-// 	(void) ucontext;
-// 	(void) info;
+/**
+* split input into words - all illegal funcs
+*/
+char **split_line(char *line)
+{
+	int bufsize = 64;
+	int i = 0;
+	char **tokens;
+	char *token;
 
-// 	if (signal == 9)
-// 		printf("no exit\n");
-// }
-
-// void	initialize_signals(void)
-// {
-// 	struct sigaction	sa;
-// 	sigset_t			set;
-
-// 	sigemptyset(&set);
-// 	sigaddset(&set, 9);
-// 	sigaddset(&set, 10);
-// 	sa.sa_mask = set;
-// 	sa.sa_flags = SA_SIGINFO;
-// 	// sa.sa_sigaction = handle_signal;
-// 	sigaction(SIGUSR1, &sa, NULL);
-// 	sigaction(SIGUSR2, &sa, NULL);
-// }
+	tokens = malloc(bufsize * sizeof(char *));
+	if (!tokens)
+	{
+		fprintf(stderr, "allocation error in split_line: tokens\n");
+		exit(EXIT_FAILURE);
+	}
+	token = strtok(line, TOK_DELIM);
+	while (token != NULL)
+	{
+		tokens[i] = token;
+		i++;
+		if (i >= bufsize)
+		{
+			bufsize += bufsize;
+			tokens = realloc(tokens, bufsize * sizeof(char *));
+			if (!tokens)
+			{
+				fprintf(stderr, "reallocation error in split_line: tokens");
+				exit(EXIT_FAILURE);
+			}
+		}
+		token = strtok(NULL, TOK_DELIM);
+	}
+	tokens[i] = NULL;
+	return (tokens);
+}
