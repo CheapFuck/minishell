2d57b95c9b9040169adc221653e45c741a598c26
rhol <rhol@student.codam.nl>
2024-06-04 13:24:43 +0200

    update

--- srcs/main.c
+++ srcs/main.c
@@ -10,6 +10,23 @@
 // 	return ;
 // }
 
+
+// static void	initialize_signals(void)
+// {
+// 	struct sigaction	sa;
+// 	sigset_t			set;
+
+// 	sigemptyset(&set);
+// 	sigaddset(&set, SIGUSR1);
+// 	sigaddset(&set, SIGUSR2);
+// 	sa.sa_mask = set;
+// 	sa.sa_flags = SA_SIGINFO;
+// 	sa.sa_sigaction = handle_signal;
+// 	sigaction(SIGUSR1, &sa, NULL);
+// 	sigaction(SIGUSR2, &sa, NULL);
+// }
+
+
 // int main(int argc, char **argv, char **envp) //envp var
 int	main(void)
 {
